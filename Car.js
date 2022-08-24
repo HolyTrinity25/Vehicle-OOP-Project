04c5e10fae5c1c742ec49dc31e4c40d433de4123
HolyTrinity25 <98700348+HolyTrinity25@users.noreply.github.com>
2022-08-24 09:26:50 -0400

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,47 @@
+class car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return "Beep.";
+    }
+    display() {
+        return `the vehicle is a ${this.make}, ${this.model} from ${this.year}.`;
+    }
+}
+
+class vehicle extends car {
+    constructor(make, model, year) {
+        super(make,model, year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends car {
+    constructor(make,model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+class Garage {
+    constructor (capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+    add(newVehicle) {
+        if(!(newVehicle instanceof vehicle)) {
+            return "Only vehicles are allowed in here!"
+        }
+        if(this.vehicles.length >= this.capacity) {
+            return "Sorry, we're full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
+}
